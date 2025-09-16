7efc1d9b9a6e480cf78f3bf3b79667975fb63c38
Ivo Lucas <ivo.lucas@makewise.pt>
2025-09-16 16:57:51 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
+#include <UIPEthernet.h> // For ENC28J60
+#include <UIPUdp.h>
 #include <AccelStepper.h>
-#include <Wire.h>
-#include <U8g2lib.h>
-// ------------------ OLED Config ------------------
-// U8g2 for SSD1306 128x64 I2C
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
 // ------------------ Hardware Config ------------------
-#define IN1 2        // L298N IN1
-#define IN2 3        // L298N IN2
-#define IN3 4        // L298N IN3
-#define IN4 5        // L298N IN4
-#define SENSOR_PIN 7 // logical sensor (marker at 0°)
+#define SENSOR_PIN 3
+
+#define IN1 4 // L298N IN1
+#define IN2 5 // L298N IN2
+#define IN3 6 // L298N IN3
+#define IN4 7 // L298N IN4
+
+#define ENA 5
+#define ENB 6
+
+// logical sensor (marker at 0°)
 
 // Stepper setup (4-wire)
-AccelStepper stepper(AccelStepper::FULL4WIRE, IN1, IN3, IN2, IN4);
+AccelStepper stepper(AccelStepper::FULL2WIRE, IN1, IN3, IN2, IN4);
 
 // ------------------ Ethernet Config ------------------
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
@@ -28,33 +29,54 @@ IPAddress remoteIp;
 unsigned int remotePort;
 
 // ------------------ Variables ------------------
-long stepsPerRevolution = 0;
-bool calibrated = false;
+volatile bool zeroMarked = false;
+volatile uint16_t stepsPerRevolution = 0;
+volatile bool calibrated = false;
 bool streaming = false;
 bool running = false;
-char lastCmd[20] = "";
+int powerLevel = 200; // 0-255
+char lastCmd[10] = "";
 
 // ------------------ Functions ------------------
+void setMotorPower(uint8_t level)
+{
+  // level: 0–255 (PWM duty cycle)
+  analogWrite(ENA, level);
+  analogWrite(ENB, level);
+}
+
+void enableMotor()
+{
+  setMotorPower(powerLevel);
+}
+
+void disableMotor()
+{
+  setMotorPower(0);
+}
+
 void calibrateStepper()
 {
+  enableMotor();
+  Serial.print("Calibration start");
+  stepper.setCurrentPosition(0);
+  stepper.stop();
+  calibrated = false;
+  zeroMarked = false;
   stepper.setSpeed(200);
-  while (digitalRead(SENSOR_PIN) == HIGH)
+
+  while (!zeroMarked)
   {
     stepper.runSpeed();
   }
-  stepper.setCurrentPosition(0);
-
-  stepsPerRevolution = 0;
+  Serial.println("Zero Found");
   stepper.setSpeed(200);
-  do
-  {
+  while (!calibrated)
     stepper.runSpeed();
-    stepsPerRevolution++;
-  } while (digitalRead(SENSOR_PIN) == HIGH);
 
-  calibrated = true;
-  Serial.print("Steps/rev: ");
+  Serial.println("Steps/rev: ");
   Serial.println(stepsPerRevolution);
+  disableMotor();
 }
 
 void sendPosition()
@@ -83,46 +105,6 @@ void sendText(const char *msg)
   Udp.endPacket();
 }
 
-void updateOLED(const char *msg)
-{
-  u8g2.firstPage();
-  do
-  {
-    u8g2.setFont(u8g2_font_5x8_tr);
-    u8g2.setCursor(0, 8);
-    u8g2.print("Stepper Controller");
-    u8g2.setCursor(0, 18);
-    u8g2.print("IP:");
-    u8g2.print(Ethernet.localIP());
-    u8g2.setCursor(0, 28);
-    u8g2.print("State:");
-    u8g2.print(running ? "Run" : "Stop");
-    u8g2.setCursor(0, 38);
-    u8g2.print("Stream:");
-    u8g2.print(streaming ? "ON" : "OFF");
-    u8g2.setCursor(0, 48);
-    u8g2.print("Speed:");
-    if (calibrated)
-    {
-      int degPerSec = (stepper.speed() * 360L) / stepsPerRevolution;
-      u8g2.print(degPerSec);
-      u8g2.print(" d/s");
-    }
-    else
-    {
-      u8g2.print("N/A");
-    }
-    u8g2.setCursor(0, 58);
-    u8g2.print("Cmd:");
-    u8g2.print(lastCmd);
-    if (msg && strlen(msg) > 0)
-    {
-      u8g2.setCursor(0, 64);
-      u8g2.print(msg);
-    }
-  } while (u8g2.nextPage());
-}
-
 void handleUDP()
 {
   int packetSize = Udp.parsePacket();
@@ -135,47 +117,74 @@ void handleUDP()
     Udp.read(incoming, packetSize);
     incoming[packetSize] = 0;
 
-    Serial.print("UDP cmd: ");
-    Serial.println(incoming);
-    strcpy(lastCmd, incoming);
+    strncpy(lastCmd, incoming, sizeof(lastCmd) - 1);
+    lastCmd[sizeof(lastCmd) - 1] = 0;
 
-    if (strcmp(incoming, "START") == 0)
+    char cmd = incoming[0]; // first letter = command
+
+    switch (cmd)
     {
-      float speed = (stepsPerRevolution / 2.0); // 1 rev / 2s
-      stepper.setSpeed(speed);
+    case 'S':                                     // Start
+      stepper.setSpeed(stepsPerRevolution / 2.0); // 1 rev / 2 sec
+      enableMotor();
       running = true;
       streaming = true;
-    }
-    else if (strcmp(incoming, "STOP") == 0)
-    {
+      break;
+
+    case 'X': // Stop
+      disableMotor();
       running = false;
-    }
-    else if (strncmp(incoming, "SPEED:", 6) == 0)
-    {
-      float degPerSec = atof(incoming + 6);
+      break;
+
+    case 'V':
+    { // Speed in deg/s
+      int degPerSec = atoi(incoming + 1);
       float stepsPerSec = (degPerSec / 360.0) * stepsPerRevolution;
       stepper.setSpeed(stepsPerSec);
+      enableMotor();
       running = true;
       streaming = true;
+      break;
     }
-    else if (strcmp(incoming, "POS?") == 0)
-    {
+
+    case 'P': // Position query
       sendPosition();
-    }
-    else if (strcmp(incoming, "STREAM:OFF") == 0)
-    {
-      streaming = false;
-    }
-    else if (strcmp(incoming, "STREAM:ON") == 0)
-    {
+      break;
+
+    case 'O': // Stream ON
       streaming = true;
-    }
-    else if (strcmp(incoming, "RESET") == 0)
-    {
+      break;
+
+    case 'F': // Stream OFF
+      streaming = false;
+      break;
+
+    case 'R': // Reset
+      enableMotor();
       running = false;
       streaming = false;
       calibrateStepper();
-      sendText("RESET OK");
+      sendText("R_OK");
+      break;
+    }
+  }
+}
+
+// ------------------ ISR ------------------
+void isrMarker()
+{
+  Serial.println("sensor");
+  if (!calibrated)
+  {
+    if (zeroMarked && stepper.currentPosition() > 20)
+    {
+      calibrated = true;
+      stepsPerRevolution = stepper.currentPosition();
+    }
+    else
+    {
+      zeroMarked = true;
+      stepper.setCurrentPosition(0);
     }
   }
 }
@@ -183,39 +192,70 @@ void handleUDP()
 // ------------------ Setup ------------------
 void setup()
 {
-  pinMode(SENSOR_PIN, INPUT_PULLUP);
+
+  TCCR0B = TCCR0B & B11111000 | B00000001; // for PWM frequency of 62500.00 Hz on D5 and D6
+  setMotorPower(0); // motor off
+  pinMode(SENSOR_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), isrMarker, RISING);
 
   Serial.begin(9600);
 
-  // OLED Init
-
-  if (!u8g2.begin())
-  {
-    Serial.println("SSD1306 allocation failed");
-    for (;;)
-      ;
-  }
-
-  // Ethernet DHCP
+  Serial.println("DHCP begin");
+  // ENC28J60 with DHCP
   if (Ethernet.begin(mac) == 0)
   {
     Serial.println("DHCP failed, fallback IP");
     IPAddress ip(192, 168, 1, 177);
     Ethernet.begin(mac, ip);
   }
-  delay(1000);
 
-  Serial.print("My IP: ");
+  switch (Ethernet.linkStatus())
+  {
+  case Unknown:
+    Serial.println("Ethernet Link Status: Unknown");
+    break;
+  case LinkON:
+    Serial.println("Ethernet Link Status: LinkON");
+    break;
+  case LinkOFF:
+    Serial.println("Ethernet Link Status: LinkOFF");
+    break;
+  default:
+    break;
+  }
+
+  switch (Ethernet.hardwareStatus())
+  {
+  case EthernetW5100:
+    Serial.println("Ethernet Hardware Status: EthernetW5100");
+    break;
+  case EthernetW5200:
+    Serial.println("Ethernet Hardware Status: EthernetW5200");
+    break;
+  case EthernetW5500:
+    Serial.println("Ethernet Hardware Status: EthernetW5500");
+    break;
+  case EthernetENC28J60:
+    Serial.println("Ethernet Hardware Status: EthernetENC28J60");
+    break;
+  default:
+    Serial.println("Ethernet Hardware Status: EthernetNoHardware");
+    break;
+  }
+
+  delay(1000);
+  Serial.print("IP: ");
   Serial.println(Ethernet.localIP());
+  Udp.begin(localPort);
 
   Udp.begin(localPort);
 
-  stepper.setMaxSpeed(2000);
+  stepper.setMaxSpeed(500);
   stepper.setAcceleration(500);
 
   calibrateStepper();
-  running = false;
-  updateOLED("Setup complete");
+
+  Serial.println("Setup complete");
 }
 
 // ------------------ Loop ------------------
@@ -231,8 +271,5 @@ void loop()
   if (streaming)
   {
     sendPosition();
-    delay(200);
   }
-
-  updateOLED(""); // refresh screen
 }
